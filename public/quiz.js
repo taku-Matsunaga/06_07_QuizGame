e6833b0b9a9d51991f690641c59fa23e6c5425a4
taku matsunaga <skyline199306@yahoo.co.jp>
2020-11-17 11:00:48 +0900

    crean js

--- public/quiz.js
+++ public/quiz.js
@@ -1,28 +1,3 @@
-// $(function () {
-
-//   // 問題の定義
-//   const question = 'コードを書くときに最も重要な要素は？？';
-//   // 正解の定義
-//   const collectAnswer = 2;
-
-//   // 読み込み時に#questionに問題を表示
-//   $('#question').text(question);
-
-//   // 選択肢クリック時に正解不正解を出力
-//   $('input[name = "choices"]').on('click', function () {
-//     if ($(this).val() == collectAnswer) {
-//       alert('ok');
-//       $('#result').text('正解');
-//     } else {
-//       alert('ng');
-//       $('#result').text('不正解');
-//     }
-//   });
-
-// });
-
-// 問題の定義
-// const question = 'コードを書くときに最も重要な要素は？？';
 
 const sockets = io();
 
@@ -75,42 +50,6 @@ function anserQuiz(countQ, checkAnser) {
     ['バルデス', 'ズレータ', 'ペドラザ'],
     ['これで', 'おしまい', 'DEATH']
   ];
-
-  // 最初の二つは除外、最後の答えは表示されない
-  // const ansers = [
-  //   // '0',
-  //   // '100',
-  //   '100',
-  //   '2',
-  //   '1',
-  //   '0',
-  //   '1',
-  //   '0'
-  // ];
-
-
-  // 正解の定義
-  // const collectAnswer = 2;
-
-  // const collectAnswer = [2, 1, 0];
-
-  // 問題数カウンタ
-  // let countQ = 0;
-
-  // if (countQ >= 0) {
-
-  //   document.getElementById('anserSubmit').onclick = function () {
-
-  //     document.getElementById('question').innerHTML = questions[countQ];
-
-  //     document.getElementById('textFirst').innerHTML = choices[countQ][0];
-  //     document.getElementById('textSecond').innerHTML = choices[countQ][1];
-  //     document.getElementById('textThird').innerHTML = choices[countQ][2];
-
-  //     console.log(`ここはインサート${countQ}`);
-  //   }
-
-  // }
 
 
   if (countQ == 0) {
@@ -192,77 +131,7 @@ function anserQuiz(countQ, checkAnser) {
 
 }
 
-// // document.getElementsByName("choices").onclick = function(){
-// const test1 = document.getElementsByName("choices");
-// const input = document.getElementsByClassName("input");
-// // console.log(test1);
 
-// test1.forEach(function(testItem){
-//   console.log(testItem);
-//   // const test2 = testItem.getElementsByClassName("input");
-//   console.log(testItem.value);
-//   if(testItem.values == collectAnswer){
-//     console.log('ok');
-//   }else{
-//     console.log('ng');
-//   }
-//   // console.log('test');
-// });
-// // test1.addEventListener("click", check);
-// input.onclick = function(){
-//   check();
-//   console.log('test');
-// }
-
-
-
-// function check(){
-//   if(this.values == collectAnswer){
-//     console.log('ok');
-//   }else{
-//     console.log('ng');
-//   }
-// }
-// };
-
-// let counting = 1;
-
-// function chatCheck(counting) {
-
-//   if (document.getElementById(`checkExist${matchCount}`) != null) {
-//     matchCountPlus = matchCount;
-
-//     let countingNumber = document.getElementById('title').textContent;
-
-//     // let counting = Number(countingNumber) - 1;
-
-//     console.log(counting);
-
-//     // +1でカウンタを調整
-//     let nameText = document.getElementById(`checkExist${counting - 1}`).textContent;
-//     // let nameTextTest = document.getElementById(`checkExist${counting}`).textContent;
-
-//     // console.log(nameText.match(ansers[matchCount]));
-
-//     console.log(`ここはカウンタ${matchCount}`);
-//     console.log(`ここはカウンティングカウンタ${counting}`);
-//     console.log(`ここはカウンタplus${matchCountPlus}`);
-//     console.log(`ここはnameText${nameText}`);
-//     console.log(`ここはansersのカウンタ${ansers[matchCount]}`);
-//     console.log(nameText.includes(ansers[matchCount]))
-
-//     if (nameText.match(ansers[matchCount])) {
-//       console.log('マッチしてます');
-//       // matchCount++;
-//     } else {
-//       console.log('マッチしてません');
-//       // matchCount++;
-//     }
-//     matchCount++;
-//     counting++;
-
-//   }
-// }
 
 
 var closeBtn = document.getElementById('closeBtn');
